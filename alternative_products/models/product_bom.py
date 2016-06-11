ad37b8d2c8be683d4a4df5efb74c2b580b53f64a
Skj <kjsarath91@gmail.com>
2016-06-11 12:10:49 +0530

    added the domain in the alternative products

--- alternative_products/models/product_bom.py
+++ alternative_products/models/product_bom.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from openerp import api, fields, models, _
-
+from openerp.osv import osv
 
 
 class Bom_Alternate_products(models.Model):
@@ -29,5 +29,22 @@ class ProductsBOM(models.Model):
 
     alternate_products =  fields.Many2many('bom.alternate.prodcts', string = 'Alternate Products')
     alternate_manufacturers =  fields.Many2many('res.partner', string = 'Alternate Manufacturers')
-    
-    
+
+    #
+
+
+    def onchange_product_id(self, cr, uid, ids, product_id, product_qty=0, context=None):
+        """ Changes UoM if product_id changes.
+        @param product_id: Changed product_id
+        @return:  Dictionary of changed values
+        """
+        res = super (ProductsBOM, self).onchange_product_id(cr, uid, ids,product_id)
+        if product_id:
+            prod = self.pool.get('product.product').browse(cr, uid, product_id)
+            al_product_ids =  [product.id for product in prod.alternative_products]
+            res.update({'domain':{
+                             'alternate_products':[('id', 'in',al_product_ids)]
+                                 }
+                        })
+
+        return res
